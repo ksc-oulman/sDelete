a2a956c8ccb75f0a9eb138673511ad6ea1fb77d0
Carlos Oulman <carlos.oulman@kingland.com>
2022-10-21 14:44:03 -0500

    Master Change drive letter for text file output

--- JobComplete.bat
+++ JobComplete.bat
@@ -25,29 +25,29 @@ set TimeStamp=%DATE:~10,4%%DATE:~4,2%%DATE:~7,2%
 
 REM Creates Network Log File
 echo. >>l:\DiskWipeResults.txt
-echo Date:     		%TimeStamp% >>l:\DiskWipeResults.txt
-echo Serial:   		%serial% >>l:\DiskWipeResults.txt
-echo Vendor:   		%compvendor% >>l:\DiskWipeResults.txt
-echo Model:    		%compname% >>l:\DiskWipeResults.txt
-echo CPU Type \ Speed:  	%CPUname% >>l:\DiskWipeResults.txt
-echo Memory:  		%Memory%MB >>l:\DiskWipeResults.txt
-echo HDD Size: 		%hdd%GB >>l:\DiskWipeResults.txt
+echo Date:     		%TimeStamp% >>C:\Wipes\DiskWipeResults.txt
+echo Serial:   		%serial% >>C:\Wipes\DiskWipeResults.txt
+echo Vendor:   		%compvendor% >>C:\Wipes\DiskWipeResults.txt
+echo Model:    		%compname% >>C:\Wipes\DiskWipeResults.txt
+echo CPU Type \ Speed:  	%CPUname% >>C:\Wipes\DiskWipeResults.txt
+echo Memory:  		%Memory%MB >>C:\Wipes\DiskWipeResults.txt
+echo HDD Size: 		%hdd%GB >>C:\Wipes\DiskWipeResults.txt
 echo ____________________________________________________________ >>l:\DiskWipeResults.txt
 
 
 REM Creates Network Label for Machine
-echo Vendor:   		%compvendor% >>l:\DiskWipe-%serial%.txt
-echo Model:    		%compname% >>l:\DiskWipe-%serial%.txt
-echo Serial:   		%serial% >>l:\DiskWipe-%serial%.txt
-echo CPU Type \ Speed:  	%CPUname% >>l:\DiskWipe-%serial%.txt
-echo Memory:  		%Memory%MB >>l:\DiskWipe-%serial%.txt
-echo HDD Size: 		%hdd%GB >>l:\DiskWipe-%serial%.txt
-echo. >>l:\DiskWipe-%serial%.txt
-echo. >>l:\DiskWipe-%serial%.txt
-echo DoD 5220.22-M sanitization Wipe using MS SDELETE - 3 Passes >>l:\DiskWipe-%serial%.txt
-echo Date Sanitzed:	%TimeStamp% >>l:\DiskWipe-%serial%.txt
-echo. >>l:\DiskWipe-%serial%.txt
-echo. >>l:\DiskWipe-%serial%.txt
+echo Vendor:   		%compvendor% >>C:\Wipes\DiskWipe-%serial%.txt
+echo Model:    		%compname% >>C:\Wipes\DiskWipe-%serial%.txt
+echo Serial:   		%serial% >>C:\Wipes\DiskWipe-%serial%.txt
+echo CPU Type \ Speed:  	%CPUname% >>C:\Wipes\DiskWipe-%serial%.txt
+echo Memory:  		%Memory%MB >>C:\Wipes\DiskWipe-%serial%.txt
+echo HDD Size: 		%hdd%GB >>C:\Wipes\DiskWipe-%serial%.txt
+echo. >>C:\Wipes\DiskWipe-%serial%.txt
+echo. >>C:\Wipes\DiskWipe-%serial%.txt
+echo DoD 5220.22-M sanitization Wipe using MS SDELETE - 3 Passes >>C:\Wipes\DiskWipe-%serial%.txt
+echo Date Sanitzed:	%TimeStamp% >>C:\Wipes\DiskWipe-%serial%.txt
+echo. >>C:\Wipes\DiskWipe-%serial%.txt
+echo. >>C:\Wipes\DiskWipe-%serial%.txt
 
 REM Creates Local Log file that displays at end of Process
 
